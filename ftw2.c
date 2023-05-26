88f1b53bc75e908dbee4371bd397208affe5ba86
shaked <shakedlevi32@gmail.com>
2023-05-26 17:19:09 +0300

    Got permmission and data of the files in the tree

--- ftw2.c
+++ ftw2.c
@@ -9,45 +9,83 @@
 */
 #define _XOPEN_SOURCE 600 /* Get nftw() */
 #include <ftw.h>
-#include <sys/types.h>    /* Type definitions used by many programs */
-#include <stdio.h>        /* Standard I/O functions */
-#include <stdlib.h>       /* Prototypes of commonly used library functions,
+#include <sys/types.h> /* Type definitions used by many programs */
+#include <stdio.h>     /* Standard I/O functions */
+#include <stdlib.h>    /* Prototypes of commonly used library functions,
                              plus EXIT_SUCCESS and EXIT_FAILURE constants */
-#include <unistd.h>       /* Prototypes for many system calls */
-#include <errno.h>        /* Declares errno and defines error constants */
-#include <string.h>       /* Commonly used string-handling functions */
+#include <unistd.h>    /* Prototypes for many system calls */
+#include <errno.h>     /* Declares errno and defines error constants */
+#include <string.h>    /* Commonly used string-handling functions */
+#include <pwd.h>
+#include <grp.h>
+#include <sys/stat.h>
 
-static int              /* Callback function called by ftw() */
+
+/**
+ * this method prints the relevant permissions of each file.
+*/
+void printPermissions(mode_t mode)
+{
+    printf((mode & S_IRUSR) ? "r" : "-");
+    printf((mode & S_IWUSR) ? "w" : "-");
+    printf((mode & S_IXUSR) ? "x" : "-");
+    printf((mode & S_IRGRP) ? "r" : "-");
+    printf((mode & S_IWGRP) ? "w" : "-");
+    printf((mode & S_IXGRP) ? "x" : "-");
+    printf((mode & S_IROTH) ? "r" : "-");
+    printf((mode & S_IWOTH) ? "w" : "-");
+    printf((mode & S_IXOTH) ? "x" : "-");
+}
+
+static int /* Callback function called by ftw() */
 dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb)
 {
-       if (type == FTW_NS) {  /* Could not stat() file */
+    if (type == FTW_NS)
+    { /* Could not stat() file */
         printf("?");
-    } else {
-        switch (sbuf->st_mode & S_IFMT) {  /* Print file type */
+    }
+    else
+    {
+        switch (sbuf->st_mode & S_IFMT)
+        { /* Print file type */
         case S_IFREG:
-            printf("%*s%s\n", 4 * ftwb->level, "|__ ", &pathname[ftwb->base]);
+        {
+            printf("%*s[", 4 * ftwb->level, ""); 
+            printPermissions(sbuf->st_mode);
+            struct passwd *owner = getpwuid(sbuf->st_uid);
+            printf(" %s ", owner ? owner->pw_name : "unknown");
+            printf("%7ld] ", (long)sbuf->st_size);
+            printf("%s\n", &pathname[ftwb->base]);
             break;
+        }
         case S_IFDIR:
-            printf("%*s%s/\n", 4 * ftwb->level, "|__ ", &pathname[ftwb->base]);
+        {
+            printf("%*s[", 4 * ftwb->level, "");
+            printPermissions(sbuf->st_mode);
+            struct passwd *owner = getpwuid(sbuf->st_uid);
+            printf(" %s ", owner ? owner->pw_name : "unknown");
+            printf("%7ld] ", (long)sbuf->st_size);
+            printf("%s/\n", &pathname[ftwb->base]);
             break;
+        }
         default:
             break;
         }
     }
-    /* Print basename */ /*TODO: here put stats of the file*/
-    return 0;                                   /* Tell nftw() to continue */
+    return 0; /* Tell nftw() to continue */ /* Tell nftw() to continue */
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int flags = 0;
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s directory-path\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if (nftw(argv[1], dirTree, 10, flags) == -1) {
+    if (nftw(argv[1], dirTree, 10, flags) == -1)
+    {
         perror("nftw");
         exit(EXIT_FAILURE);
     }
