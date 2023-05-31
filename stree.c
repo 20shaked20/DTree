6335b9278051f1bbfd96615328680a1411e0e943
shaked <shakedlevi32@gmail.com>
2023-05-31 11:57:13 +0300

    Changes to buildup of tree and some notations

--- stree.c
+++ stree.c
@@ -19,8 +19,12 @@
 #include <pwd.h>
 #include <grp.h>
 #include <sys/stat.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <limits.h>
 
-
+bool lastWasDir = false; // a simple check if the last file was direcotry and now we're in a normal file//
+int dirFiles = 0;
 /**
  * This method gets the mode of a file and prints the permission of it.
 */
@@ -37,35 +41,95 @@ void printPermissions(mode_t mode)
     printf((mode & S_IXOTH) ? "x" : "-");
 }
 
-static int dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb)
+int countFilesInDirectory(const char *dirname)
 {
+    int count = 0;
+    DIR *dir = opendir(dirname);
+    if (dir != NULL)
+    {
+        struct dirent *entry;
+        while ((entry = readdir(dir)) != NULL)
+        {
+            char path[PATH_MAX];
+            snprintf(path, sizeof(path), "%s/%s", dirname, entry->d_name);
+            
+            struct stat fileStat;
+            if (stat(path, &fileStat) == 0 && S_ISREG(fileStat.st_mode))
+            {
+                count++;
+            }
+        }
+        closedir(dir);
+    }
+    return count;
+}
+
+
+static int dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb)
+{   
+
     if (type == FTW_NS)
     {
         printf("?");
     }
     else
     {
+        // for(int i = 0; i< ftwb->level; ++i)
+        // {
+        //     // if(i == ftwb->level)
+        //         printf("%d",i);
+        // }
+        printf("%*s", 4 * ftwb->level - ftwb->level, ""); //indent//
+        
+        // for(int i = 0; i< ftwb->level; ++i)
+        // {
+        //     // if(i == ftwb->level)
+        //         printf("%*s", 1/4 * ftwb->level - ftwb->level -1 , ""); //indent few backwards to fit good//
+        //         // printf("│");
+        // }
+        // printf("%*s", ftwb->level - ftwb->level, ""); //indent//
+
         switch (sbuf->st_mode & S_IFMT)
         {
         case S_IFREG: // reg file.
-        {
-            printf("%*s|-- [", 4 * ftwb->level, ""); //indent//
+        {   
+            --dirFiles; // remove file from current directory // 
+
+            if((lastWasDir && dirFiles == 0) || dirFiles == 0){
+                printf("└──["); 
+            } 
+            else{
+                 printf("├──["); 
+            }
+
             printPermissions(sbuf->st_mode); // prints [xcvxc--xcvxv--xcvxcv]
             struct passwd *owner = getpwuid(sbuf->st_uid); // gets the owner
             printf(" %s %7ld] ", owner ? owner->pw_name : "unknown", (long)sbuf->st_size); // prints owner and ends of the needed data
             printf("%s\n", &pathname[ftwb->base]); // prints file name
+            lastWasDir = false;
             break;
         }
-        case S_IFDIR: // directory only// 
-        {
-            if (ftwb->level > 0)
-                printf("%*s|-- [", 4 * ftwb->level - 4, ""); //indent if level 0, to start better//
-            else
-                printf("%*s|-- [", 4 * ftwb->level, ""); //indent//
+        case S_IFDIR: // directory only//
+        {  
+
+            if (ftwb->level == 0){ // for the first file
+                printf(".\n");
+                printf("└──["); 
+            }
+            else{
+                printf("├──[");
+            }
+             
             printPermissions(sbuf->st_mode); // prints [xcvxc--xcvxv--xcvxcv]
             struct passwd *owner = getpwuid(sbuf->st_uid); // gets the owner
             printf(" %s %7ld] ", owner ? owner->pw_name : "unknown", (long)sbuf->st_size); // prints owner and ends of the needed data
             printf("%s/\n", &pathname[ftwb->base]); // prints file name
+            lastWasDir = true;
+
+            /*we do this to get the amount of files in directory for smooth printing*/
+            const char *dir = pathname;
+            dirFiles = countFilesInDirectory(pathname);
+
             break;
         }
         default:
@@ -74,6 +138,8 @@ static int dirTree(const char *pathname, const struct stat *sbuf, int type, stru
     }
     return 0;
 }
+
+
 
 int main(int argc, char *argv[])
 {
@@ -84,7 +150,8 @@ int main(int argc, char *argv[])
     }
 
     int flags = FTW_PHYS;
-    if (nftw(argv[1], dirTree, 10, flags) == -1)
+    int maxDepth = 10;
+    if (nftw(argv[1], dirTree, maxDepth, flags) == -1)
     {
         perror("nftw");
         exit(EXIT_FAILURE);
